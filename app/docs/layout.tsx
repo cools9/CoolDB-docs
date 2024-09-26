0f1885280ac3237b4346695852d3fdac8ff28ad8
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-09-27 07:10:58 +0800

    changes in MDX files

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -2,6 +2,16 @@ import { DocsLayout } from 'fumadocs-ui/layout';
 import type { ReactNode } from 'react';
 import { baseOptions } from '../layout.config';
 import { source } from '@/app/source';
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "LunarDB",
+  description: "Documentation & Blog for LunarDB",
+  icons: {
+    icon: "/public/favicon.ico",
+    // apple: "/apple-icon.png",
+  },
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,20 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
+import type { Metadata } from "next"
 
 const inter = Inter({
   subsets: ['latin'],
 });
+
+export const metadata: Metadata = {
+  title: "LunarDB",
+  description: "Documentation & Blog for LunarDB",
+  icons: {
+    icon: "/public/favicon.ico",
+    // apple: "/apple-icon.png",
+  },
+}
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
