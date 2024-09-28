2e982dcfa9323c2e29bb6c61c7a4e8102909d571
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-09-28 20:15:47 +0800

    changes on layout.tsx files

---
+++ app/security/layout.tsx
@@ -0,0 +1,22 @@
+import { DocsLayout } from "fumadocs-ui/layout";
+import type { ReactNode } from "react";
+import { baseOptions } from "../layout.config";
+import { source } from "@/app/source";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "LunarDB",
+  description: "Documentation & Blog for LunarDB",
+  icons: {
+    icon: "/lunaricon.svg",
+    // apple: "/apple-icon.png",
+  },
+};
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <DocsLayout tree={source.pageTree} {...baseOptions}>
+      {children}
+    </DocsLayout>
+  );
+}
