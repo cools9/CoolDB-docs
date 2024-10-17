4d17d2e3021f16d0d024de7c71fda2c07c6f604a
Bashamega <adambashaahmednaji@gmail.com>
2024-10-17 15:26:22 +0300

    fix: lighmode

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <footer className="bg-gray-800 text-white p-4 text-center">
+        <footer className="bg-gray-800 bg-opacity-20 p-4 text-center">
             <p className="text-sm">
                 © {new Date().getFullYear()} Lunar DB. All rights reserved.
             </p>
